d28c66472eca11d5933c8271b97d9113ae4535bb
SkylerGubbels <skylermgubbels@gmail.com>
2019-03-11 12:48:44 -0400

    Added all client attributes

--- deliverable2-code/client.cpp
+++ deliverable2-code/client.cpp
@@ -52,3 +52,42 @@ std::string Client::getAddress() { return address; }
 std::string Client::getPhoneNum() { return phone; }
 std::string Client::getCity() { return city; }
 std::string Client::getProvince() { return province; }
+
+void Client::setClientAttributes(std::string firstName, std::string lastName, std::string address, std::string phone,
+                    std::string email, std::string city, std::string province, int dwelling, int location, int workSchedule,
+                    int activity, bool hasChildren, bool hasAnimals, int travels, int children, int goodWAnimals, int strangers,
+                    int crowds, int noises, int protector, int energy, int fearful, int affection, int messy,
+                    bool wantsDog, bool hasDogAllergies, std::vector<std::string> dogBreeds, int dogAge, int dogSize, int dogGender,
+                            int followsCommandsDog, int houseTrained,
+                    bool wantsCat, bool hasCatAllergies, std::vector<std::string> catBreeds, int catAge, int catSize, int catGender,
+                            int isCurious, int followCommandsCat, int doesntShed,
+                    bool wantsBird, bool hasBirdAllergies, std::vector<std::string> birdBreeds, int birdAge, int birdSize, int birdGender,
+                            int isQuietBird, int isSocialBird, std::string birdColour,
+                    bool wantsLizard, bool hasLizardAllergies, std::vector<std::string> lizardBreeds, int lizardAge, int lizardSize, int lizardGender,
+                            int easyToFeed, int simpleLiving, std::string lizardColour,
+                    bool wantsRabbit, bool hasRabbitAllergies, std::vector<std::string> rabbitBreeds, int rabbitAge, int rabbitSize, int rabbitGender,
+                            int isSocialRabbit, int needsGrooming, std::string rabbitColour)
+{
+    setContactInformation(firstName, lastName, address, phone, email, city, province);
+    this->dwelling = dwelling; this->location = location; this->workSchedule = workSchedule; this->activity = activity;
+    this->hasChildren = hasChildren; this->hasAnimals = hasAnimals; this->travels = travels; this->children = children;
+    this->goodWAnimals = goodWAnimals; this->strangers = strangers; this->crowds = crowds; this->noises = noises; this->protector = protector;
+    this->energy = energy; this->fearful = fearful; this->affection = affection; this->messy = messy;
+
+    this->wantsDog = wantsDog; this->hasDogAllergies = hasDogAllergies; this->dogBreeds = dogBreeds; this->dogAge = dogAge; this->dogSize = dogSize;
+    this->dogGender = dogGender; this->followsCommandsDog = followsCommandsDog; this->houseTrained = houseTrained;
+
+    this->wantsCat = wantsCat; this->hasCatAllergies = hasCatAllergies; this->catBreeds = catBreeds; this->catAge = catAge;
+    this->catSize = catSize; this->catGender = catGender; this->isCurious = isCurious; this->followCommandsCat = followCommandsCat;
+    this->doesntShed = doesntShed;
+
+    this->wantsBird = wantsBird; this->hasBirdAllergies = hasBirdAllergies; this->birdBreeds = birdBreeds; this->birdAge = birdAge; this->birdSize = birdSize;
+    this->birdGender = birdGender; this->isQuietBird = isQuietBird; this->isSocialBird = isSocialBird; this->birdColour = birdColour;
+
+    this->wantsLizard = wantsLizard; this->hasLizardAllergies = hasLizardAllergies; this->lizardBreeds = lizardBreeds; this->lizardAge = lizardAge;
+    this->lizardSize = lizardSize; this->lizardGender = lizardGender; this->easyToFeed = easyToFeed; this->simpleLiving = simpleLiving; this->lizardColour = lizardColour;
+
+    this->wantsRabbit = wantsRabbit; this->hasRabbitAllergies = hasRabbitAllergies; this->rabbitBreeds = rabbitBreeds; this->rabbitAge = rabbitAge; this->rabbitSize = rabbitSize;
+    this->rabbitGender = rabbitGender; this->isSocialRabbit = isSocialRabbit; this->needsGrooming = needsGrooming; this->rabbitColour = rabbitColour;
+
+}
--- deliverable2-code/client.h
+++ deliverable2-code/client.h
@@ -2,6 +2,7 @@
 #define CLIENT_H
 
 #include <string>
+#include <vector>
 
 class Client
 {
@@ -23,11 +24,67 @@ public:
     std::string getCity();
     std::string getProvince();
 
+    void setClientAttributes(std::string firstName, std::string lastName, std::string address, std::string phone,
+                        std::string email, std::string city, std::string province, int dwelling, int location, int workSchedule,
+                        int activity, bool hasChildren, bool hasAnimals, int travels, int children, int goodWAnimals, int strangers,
+                        int crowds, int noises, int protector, int energy, int fearful, int affection, int messy,
+                        bool wantsDog, bool hasDogAllergies, std::vector<std::string> dogBreeds, int dogAge, int dogSize, int dogGender,
+                                int followsCommandsDog, int houseTrained,
+                        bool wantsCat, bool hasCatAllergies, std::vector<std::string> catBreeds, int catAge, int catSize, int catGender,
+                                int isCurious, int followCommandsCat, int doesntShed,
+                        bool wantsBird, bool hasBirdAllergies, std::vector<std::string> birdBreeds, int birdAge, int birdSize, int birdGender,
+                                int isQuietBird, int isSocialBird, std::string birdColour,
+                        bool wantsLizard, bool hasLizardAllergies, std::vector<std::string> lizardBreeds, int lizardAge, int lizardSize, int lizardGender,
+                                int easyToFeed, int simpleLiving, std::string lizardColour,
+                        bool wantsRabbit, bool hasRabbitAllergies, std::vector<std::string> rabbitBreeds, int rabbitAge, int rabbitSize, int rabbitGender,
+                                int isSocialRabbit, int needsGrooming, std::string rabbitColour);
+
 private:
     std::string firstName, lastName, address,
         phone, email, city, province;
     int idNumber;
     int CLIENT_STORAGE_CODE = 1;
+
+    // Client attributes
+    int dwelling, location, workSchedule, activity;
+    bool hasChildren, hasAnimals;
+
+    // General Animal Attributes
+    int travels, children, goodWAnimals, strangers, crowds,
+        noises, protector, energy, fearful, affection, messy;
+
+    // Dog
+    bool wantsDog, hasDogAllergies;
+    std::vector<std::string> dogBreeds;
+    int dogAge, dogSize, dogGender, dogFur; // Physical
+    int quietness, followsCommandsDog, houseTrained; // Importance of
+
+    // Cat
+    bool wantsCat, hasCatAllergies;
+    std::vector<std::string> catBreeds;
+    int catAge, catGender, catSize, catFur; // Physical
+    int isCurious, followCommandsCat, doesntShed; // Importance of
+
+    // Bird
+    bool wantsBird, hasBirdAllergies;
+    std::vector<std::string> birdBreeds;
+    int birdAge, birdGender, birdSize, birdFur; // Physical
+    std::string birdColour;
+    int isQuietBird, isSocialBird; // Importance of
+
+    // Lizard
+    bool wantsLizard, hasLizardAllergies;
+    std::vector<std::string> lizardBreeds;
+    int lizardAge, lizardGender, lizardSize, lizardFur; // Physical
+    std::string lizardColour;
+    int easyToFeed, simpleLiving; // Importance of
+
+    // Rabbit
+    bool wantsRabbit, hasRabbitAllergies;
+    std::vector<std::string> rabbitBreeds;
+    int rabbitAge, rabbitGender, rabbitSize, rabbitFur; // Physical
+    std::string rabbitColour;
+    int isSocialRabbit, needsGrooming; // Importance of
 
 };
 
