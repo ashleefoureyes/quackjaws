4324fc994849a1749249b016434bb159c0dec154
Ashlee <ashleefoureyes@gmail.com>
2019-04-07 20:06:46 -0400

    client categorization

--- deliverable4-code/algorithm.h
+++ deliverable4-code/algorithm.h
@@ -18,6 +18,7 @@ public:
 private:
     double computeDistance(Animal*, Client*);
     double categorize(Animal*);
+    double categorize(Client*);
     void computeOptimalMatches(std::map<int, std::vector<Match*>> *matches, std::vector<Match*> *optimalMatches);
     std::map<int, std::vector<Match*>> countMatches (std::map<int, std::vector<Match*>> *matches, double matchThreshold);
     void makeMatch(std::map<int, std::vector<Match*>> *matches, std::vector<Match*> *optimalMatches, int clientId, Match *match, std::map<int, std::vector<Match*>> *matchCounts);
